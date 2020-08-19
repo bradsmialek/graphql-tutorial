711fcc69dbbe1226de8074ca3e2053dae384ab5b
bradsmialek <bradsmilek@gmail.com>
2020-08-19 15:37:06 -0700

    added auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Booklist from './components/booklist'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <h1>React Front End</h1>
+      <header>
+        <h1>React Front End</h1>
+      </header>
+      
       <Booklist/>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+// export default App;
+export default withAuthenticator(App);
